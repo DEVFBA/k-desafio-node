d8a60aad48ffa60442fe1dce06f767311c387bd0
Leonardo Meza <leonardomp2200@gmail.com>
2024-02-11 22:32:20 -0600

    feat: Modal con funcion create

--- js/create.js
+++ js/create.js
@@ -4,6 +4,7 @@ import { createPrincipalCard } from "./html.js";
 const create = (id, principalImage, profileImage1, profileImage2, author, date, title, hashtags) => {
 
     const centerSection = document.getElementById('centerSection');
+    
 
     createPrincipalCard(centerSection, id, principalImage, profileImage1, profileImage2, author, date, title, hashtags);
 
--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ const post = {
 	minRead: 7
 }
 
-const id = 987654;
+//const id = 987654;
 
 /**
  * Create
@@ -70,25 +70,28 @@ readCards(0);
  */
 
 const newPostBtn = document.getElementById('new-post');
-const btnSearch = document.getElementById('search')
-const inputSearch = document.getElementById('inputSearch')
+
 inputSearch.addEventListener('change', () => {
+	const btnSearch = document.getElementById('search')
+	const inputSearch = document.getElementById('inputSearch')
 	inputSearch.value
 	console.log(inputSearch.value)
 	readCards(2, null, `${inputSearch.value}` )
 })
 
-const inputTitle = document.getElementById('input-title')
-const inputAuthor = document.getElementById('input-author')
-const inputHashtags = document.getElementById('input-#')
-const inputText = document.getElementById('input-text')
 const btnCreate = document.getElementById('create')
 btnCreate.addEventListener('click', ()=> {
-	console.log(inputTitle.value)
-	console.log(inputAuthor.value)
-	console.log(inputHashtags.value)
-	console.log(inputText.value)
-	create(inputTitle.value)
+	const inputTitle = document.getElementById('input-title')
+	const inputAuthor = document.getElementById('input-author')
+	const inputHashtags = document.getElementById('input-#')
+	const inputText = document.getElementById('input-text')
+	
+	const id = Date.now()
+	const image = 'https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fi2pusd0be8bwn7nz4a03.jpg'
+	const profileImage1 = 'https://media.dev.to/cdn-cgi/image/width=90,height=90,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Fuser%2Fprofile_image%2F1269538%2F59f83787-8a6b-495b-85a4-c8b25fd2a1b4.jpeg'
+	const profileImage2 = 'https://res.cloudinary.com/practicaldev/image/fetch/s--ZMDQfkJE--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/1177677/4947a9f8-ba30-4008-9f77-4b08c5b1c901.jpg'
+	const fecha = '2024-01-31'
+	create(id, image, profileImage1, profileImage2, inputAuthor.value, fecha, inputTitle.value, inputHashtags.value)
 
 })
 
